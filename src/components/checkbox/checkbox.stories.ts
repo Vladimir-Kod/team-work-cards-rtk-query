23b2200ef35f1a1b22a9e5ba70ba0a173aa02425
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-09-28 12:05:38 +0300

    test commit

---
+++ src/components/checkbox/checkbox.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Checkbox from "@/components/checkbox/checkbox.tsx";
+
+
+const meta = {
+    title: 'Components/Checkbox',
+    component: Checkbox,
+    tags: ['autodocs'],
+    argTypes: {
+        variant: {
+            options: ['primary', 'secondary', 'tertiary', 'link'],
+            control: { type: 'radio' },
+        },
+    },
+} satisfies Meta<typeof Checkbox>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+    args: {
+
+    },
+}
